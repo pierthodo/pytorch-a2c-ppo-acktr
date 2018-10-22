8727c2c5b1ac21b97fc021a49def102027622e04
pierthodo <pierthodo@gmail.com>
2018-10-22 12:53:42 -0400

    fix prev_value

--- model.py
+++ model.py
@@ -105,7 +105,7 @@ class Policy(nn.Module):
         dist_entropy = dist.entropy().mean()
         print("beta",beta_v)
         print("Indices ",indices)
-        print(value[indices])
+        print(value[l])
         print(value_mixed)
         return value_mixed, action_log_probs, dist_entropy, rnn_hxs
 
